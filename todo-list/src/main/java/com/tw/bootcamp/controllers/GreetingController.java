cc4fa6b84846e14d1dd8c8ed3e5d98367db26aa6
silverjava <silverjava@gmail.com>
2016-11-14 09:16:28 +0800

    remove hateoas

--- todo-list/src/main/java/com/tw/bootcamp/controllers/GreetingController.java
+++ todo-list/src/main/java/com/tw/bootcamp/controllers/GreetingController.java
@@ -1,6 +1,5 @@
 package com.tw.bootcamp.controllers;
 
-import com.tw.bootcamp.resources.Greeting;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -18,11 +18,10 @@ public class GreetingController {
 
     @RequestMapping("/")
     @ResponseBody
-    public HttpEntity<Greeting> index(
+    public HttpEntity<Map<String, String>> index(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-        Greeting greeting = new Greeting(String.format(GREETING_TEMPLATE, name));
-        greeting.add(linkTo(methodOn(GreetingController.class).index(name)).withSelfRel());
-
-        return new ResponseEntity<>(greeting, OK);
+        HashMap<String, String> result = new HashMap<>();
+        result.put("content", String.format(GREETING_TEMPLATE, name));
+        return new ResponseEntity<>(result, OK);
     }
 }
