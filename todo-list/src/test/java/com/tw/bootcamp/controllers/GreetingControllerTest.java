9fbdd822c765619eaae01cad437b2ca6dd7e2b4a
silverjava <silverjava@gmail.com>
2016-11-17 13:55:23 +0800

    refactoring test

--- todo-list/src/test/java/com/tw/bootcamp/controllers/GreetingControllerTest.java
+++ todo-list/src/test/java/com/tw/bootcamp/controllers/GreetingControllerTest.java
@@ -1,25 +1,21 @@
-package com.tw.bootcamp;
+package com.tw.bootcamp.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.Is.is;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
-public class TodoAppTest {
-
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
